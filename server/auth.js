4c3600d6c259b7936ceeeef21c88f236bc2dc7bf
riy0 <chac0ptx@gmail.com>
2020-02-25 10:42:07 +0900

    add/auth

--- server/auth.js
+++ server/auth.js
@@ -1,1 +1,15 @@
-const 
+const config = require('./config.js');
+
+const GoogleOAuthStrategy = require('passport-google-oauth20').Strategy;
+module.exports = (passport) => {
+  passport.serializeUser((user, done) => done(null, user));
+  passport.deserializeUser((user, done) => done(null, user));
+  passport.use(new GoogleOAuthStrategy(
+      {
+        clientID: config.oAuthClientID,
+        clientSecret: config.oAuthclientSecret,
+        callbackURL: config.oAuthCallbackUrl,
+        userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
+      },
+      (token, refreshToken, profile, done) => done(null, {profile, token})));
+};
